a315944acdc11b8ee2d2756568e1f2d3cc60c765
GenJay99 <jasongencarella1@gmail.com>
2019-09-25 12:45:56 -0400

    Level 3 - database encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,12 +20,17 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useUnifiedTopology: true
 });
 
-const userScema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
 
-const User = new mongoose.model("User", userScema);
+// Create a secret to encrypt with, then create the nessessary plugin to execute for the Password
+// field.
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
 
 
 
