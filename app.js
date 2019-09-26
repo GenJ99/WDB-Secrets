281c90420648bac7bcacefba36fb178cee425a17
GenJay99 <jasongencarella1@gmail.com>
2019-09-26 08:43:31 -0400

    Level 3 - hashing passwords

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-// dotenv API key example writen to the command line
-console.log(process.env.API_KEY);
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -29,9 +26,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-// Create a dotenv secret to encrypt with, then create the nessessary plugin to execute for the Password
-// field.
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,13 +38,36 @@ app.get("/", function(req, res) {
 
 
 
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    // MD5 function turns the user password into an irreversible hash for increased security.
+    password: md5(req.body.password)
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+
+
 app.get("/login", function(req, res) {
   res.render("login");
 });
 
 app.post("/login", function(req, res) {
   const userName = req.body.username;
-  const password = req.body.password;
+  // MD5 function used to match the login password to the register password in the database.
+  const password = md5(req.body.password);
 
   User.findOne({email: userName}, function(err, foundUser) {
     if (err) {
@@ -60,27 +78,6 @@ app.post("/login", function(req, res) {
           res.render("secrets");
         }
       }
-    }
-  });
-});
-
-
-
-app.get("/register", function(req, res) {
-  res.render("register");
-});
-
-app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
     }
   });
 });
